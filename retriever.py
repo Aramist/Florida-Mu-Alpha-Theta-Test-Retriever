dc935f49f013f5d17905dfa49fa3d1f3498d2b20
Aramis Tanelus <bd543491@ahschool.com>
2016-09-06 23:41:06 -0400

    Updated retriever.py and tests.json
    I honestly don't remember everything i changed in retriever.py. Added
    all algebra II and Geometry tests to tests.json

--- retriever.py
+++ retriever.py
@@ -4,6 +4,7 @@ from urllib import request
 import os
 from os import path
 import difflib
+import re
 from PyPDF2 import PdfFileMerger as Merger
 
 class Test:
@@ -30,8 +31,12 @@ class Subject:
         self.name = data["aliases"][0]
         self.aliases = data["aliases"]#[1:len(data["aliases"])]
         self.tests = [Test(a) for a in data["tests"]]
+        self.testdict = dict()
+        for test in self.tests:
+            pass
         self.cache_tests()
     def cache_tests(self):
+        print("Caching Tests...")
         if not os.path.exists(self.name+"/"):
             os.makedirs(self.name + "/")
         for t in self.tests:
@@ -40,13 +45,11 @@ class Subject:
                 testfile = open(self.name + "/" + t.testname, "wb")
                 testfile.write(testdata)
                 testfile.close()
-                testdata.close()
             if not os.path.isfile(self.name + "/" + t.solutionsname):
                 testdata = urllib.request.urlopen(t.solutions).read() if t.solutions != "" else b''
                 testfile = open(self.name + "/" + t.solutionsname, "wb")
                 testfile.write(testdata)
                 testfile.close()
-                testdata.close()
     def as_string(self):
         fdict = {"aliases": str(self.aliases), "name": str(self.name), "tests": str(len(self.tests))}
         return "{name}:\n\tAliases: {aliases}\n\tTests: {tests}".format(**fdict)
@@ -71,7 +74,7 @@ testdata = json.loads(open("tests.json", "r").read())
 
 calculusdata = testdata["calculus"]
 precalculusdata = testdata["precalculus"]
-algebradata = testdata["algebraII"]
+algebradata = testdata["algebra"]
 geometrydata = testdata["geometry"]
 
 calculus = Subject(calculusdata)
@@ -90,16 +93,33 @@ class InterestObject:
     def __init__(self, desc = '', index = 0, data = None):
         self.description = desc
         self.index = index
+        self.data = data
 
 def run():
     query = input("Request: ")
     sentence = query.split(" ")
-    similarwords = {"test":["tests","quiz","quizzes"]}
-
+    similarwords = {"test":["tests","test","quiz","quizzes"]}
     keywords = ['all','and','from','through', 'to', 'with', 'without']
     interest = []
-
-
+    for word in sentence:
+        for s in subjects:
+            for a in s.aliases:
+                if s.name == 'calculus' or s.name == 'precalculus':
+                    print("Discerning between precalc and calc")
+                    print("Chance of " + s.name + ': ' + str(str_ratio(word, a)))
+                    if str_ratio(word, a) >= 0.90:
+                        print('\nreplacing\n')
+                        query = query.replace(word, s.name)
+                        continue
+                if str_ratio(word, a) >= 0.7:
+                    query = query.replace(word, s.name)
+    for s in subjects:
+        for a in similarwords["test"]:
+            if s.name + ' ' + a in query:
+                query = query.replace(s.name + ' ' + a, s.name)
+    sentence = query.split(' ')
+    print("First process: " + query)
+    similaritydict = {s.name: max([str_ratio(w, a) for a in s.aliases for w in sentence]) for s in subjects}
     for word in sentence:
         length = len(sentence)
         index = sentence.index(word)
@@ -108,13 +128,10 @@ def run():
                 interest.append(InterestObject('all', index, sentence[index:]))
                 continue
             if word.lower() == "and" and not index == length - 1:
-                interest.append(InterestObject('and', index, sentence[:index]+sentence[index+1:]
+                interest.append(InterestObject('and', index, sentence[:index]+sentence[index+1:]))
                 continue
-            if index < length - 1 and not index == 0:
-                interest.append(InterestObject(word.lower, index, (sentence[index-1],sentence[index+1])
-            
-            
-
+            if (index < (length - 1) and not index == 0):
+                interest.append(InterestObject(word.lower(), index, (sentence[index-1],sentence[index+1])))
 
     similaritydict = {s.name: max([str_ratio(w, a) for a in s.aliases for w in sentence]) for s in subjects}
 
@@ -126,12 +143,15 @@ def run():
             interest.append(InterestObject(a + " found in sentence", indexOf(a, sentence)))
 
     print("Interests")
-    [print("\t" + str(i.index) + ": " + i.description) for i in interest]
+    [print("\t" + str(i.index) + ": " + i.description + '    -    ' + str(i.data)) for i in interest]
 
-    for n in interests:
+    for n in interest:
         if 'comma' in n.description.lower():
+            pass
         if 'all' in n.description.lower():
+            pass
         if 'and' in n.description.lower():
+            pass
 
 if __name__ == '__main__':
     run()
